5d74a64b40afbb3364f8ae42fedc3838ee0809f5
Ivan <voodoolar333@gmail.com>
2023-07-20 10:38:02 +0200

    get Data randomChar

--- src/components/randomChar/randomChar.js
+++ src/components/randomChar/randomChar.js
@@ -1,33 +1,90 @@
-import './randomChar.scss'
-import thor from '../../resurses/img/Thumbnail.png'
-import mjolnir from '../../resurses/img/mjolnir.png'
-import shield from '../../resurses/img/shield.png'
+import { Component } from 'react'
 
-const RandomChar = () => {
-    return (
-        <div className="randomchar">
-            <div className="randomchar__block">
-                <img src={thor} alt="thor" className="randomchar__block-img" />
-                <div className="randomchar__block-info">
-                    <div className="randomchar__name">THOR</div>
-                    <div className="randomchar__descr">As the Norse God of thunder and lightning, Thor wields one of the greatest weapons ever made, the enchanted hammer Mjolnir. While others have described Thor as an over-muscled, oafish imbecile, he's quite smart and compassionate...</div>
-                    <div className="randomchar__btns">
-                        <div className="randomchar__btn">
-                            <a href="#" className="button button_red button_main">HOMEPAGE</a>
-                        </div>
-                        <div className="randomchar__btn">
-                            <a href="#" className="button button_grey button_main">WIKI</a>
-                        </div>
-                    </div>
+import MarvelService from '../../services/marvelService';
+import Spinner from '../spinner/spinner';
+import ErrorMessage from '../errorMessage/errorMessage';
+
+import './randomChar.scss';
+import mjolnir from '../../resurses/img/mjolnir.png';
+import shield from '../../resurses/img/shield.png';
+
+class RandomChar extends Component {
+
+    state = {
+        char: {},
+        loading: true,
+        error: false
+    }
+
+    marvelService = new MarvelService();
+
+    onCharLoaded = (char) => {
+        this.setState({
+            char,
+            loading: false
+        })
+    }
+
+    onError = () => {
+        this.setState({
+            loading: false,
+            error: true
+        })
+    }
+
+    updateChar = () => {
+        const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
+        this.marvelService
+            .getCharacter(id)
+            .then(this.onCharLoaded)
+            .catch(this.onError);
+    }
+
+    componentDidMount() {
+        this.updateChar();
+    }
+
+
+    render () {
+        const {char, loading, error} = this.state;
+        const errorMessage = error ? <ErrorMessage/> : null;
+        const spinner = loading ? <Spinner/> : null;
+        const content = !(loading || error) ? <View char={char}/> : null;
+
+        return (
+            <div className="randomchar">
+                {errorMessage}
+                {spinner}
+                {content}
+
+                <div className="randomchar__static">
+                    <div className="randomchar__static-info">Random character for today!<br />Do you want to get to know him better?</div>
+                    <div className="randomchar__static-random">Or choose another one</div>
+                    <div className="button button_red button_main" onClick={this.updateChar}>TRY IT</div>
+                    <img src={mjolnir} alt="mjolnir" className="randomchar__mjolner" />
+                    <img src={shield} alt="shield" className="randomchar__shield" />
                 </div>
             </div>
+        )
+    }
+}
 
-            <div className="randomchar__static">
-                <div className="randomchar__static-info">Random character for today!<br />Do you want to get to know him better?</div>
-                <div className="randomchar__static-random">Or choose another one</div>
-                <div className="button button_red button_main">TRY IT</div>
-                <img src={mjolnir} alt="mjolnir" className="randomchar__mjolner" />
-                <img src={shield} alt="shield" className="randomchar__shield" />
+const View = ({char}) => {
+    const {name, thumbnail, description, homepage, wiki} = char;
+    return (
+        <div className="randomchar__block">
+            <img src={thumbnail} alt={name} className="randomchar__block-img" />
+            <div className="randomchar__block-info">
+            <div className="randomchar__name">{name}</div>
+                <div className="randomchar__descr">{description}</div>
+                <div className="randomchar__btns">
+                    <div className="randomchar__btn">
+                        <a href={homepage} className="button button_red button_main">HOMEPAGE</a>
+                    </div>
+                    <div className="randomchar__btn">
+                        <a href={wiki} className="button button_grey button_main">WIKI</a>
+                    </div>
+                </div>
             </div>
         </div>
     )
